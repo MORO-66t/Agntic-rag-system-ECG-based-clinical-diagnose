888ac5ac62d6d72bda7181d13d21cb6293272814
MORO-66t <166075118+MORO-66t@users.noreply.github.com>
2026-06-04 02:02:52 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-HF_TOKEN = "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"
-HF_TOKENS = [
-    "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",
-    "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",
-    "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",
-    "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"
-]
-MODEL_NAME = "Qwen/Qwen2.5-7B-Instruct"
